c354a5efc3a94b377ee8f89fee950f91d628d25d
pushpa rajput <pr4541285@gmail.com>
2024-07-25 15:05:36 +0530

    add utils

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR :", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at port :  ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed!!!!", err);
+  });
 
 //************  first approach ******************** */
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+//**************by promises */
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//****************by tryCatch */
+
+// const asyncHandler=()=>{}
+// const asyncHandler=(func)=>{()=>{}}
+// const asyncHandler=(func)=>async()=>{}     explaination
+
+//const asyncHandler=(fn)=>async(error,req,res,next)=>{}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 3000).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
